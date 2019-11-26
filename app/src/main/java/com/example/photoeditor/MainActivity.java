0ff3a0244116561047bd32755e0372b366ec9717
Nikolay <nicola.mosseev@yandex.ru>
2019-11-26 06:55:47 +0300

    Updating permissions request and design update

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.photoeditor;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -24,36 +29,56 @@ public class MainActivity extends AppCompatActivity {
     String currentImageName;
     private Uri outputFileUri;
 
+    //
+    private static final int REQUEST_PERMISSIONS = 111;
+    //Разрешения для записи и чтения
+    private static final String[] PERMISSONS = {
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Существует ли камера
+        if(!MainActivity.this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            findViewById(R.id.btnTakePhoto).setVisibility(View.GONE);
+        }
+
         TextView txtView = findViewById(R.id.text);
         txtView.setText(strSDCardPathName);
         createFolder();
 
-        Button btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        final Button btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        final Button btnSelectImage = findViewById(R.id.btnSelectImage);
 
         btnTakePhoto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 /*Переход в режим камеры*/
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) { }
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) { }
 
-                    outputFileUri = Uri.fromFile(photoFile);
-                    if (photoFile != null) {
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-                        startActivityForResult(takePictureIntent, 1);
+                outputFileUri = Uri.fromFile(photoFile);
+                if (photoFile != null) {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                    startActivityForResult(takePictureIntent, 1);
 
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Произошла ошибка", Toast.LENGTH_SHORT);
                 }
+
+                //Toast.makeText(MainActivity.this, "Произошла ошибка", Toast.LENGTH_SHORT);
+
+            }
+        });
+
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
             }
         });
     }
@@ -90,5 +115,38 @@ public class MainActivity extends AppCompatActivity {
              }
         } catch (Exception ex) {
         }
+    }
+
+    @SuppressLint("NewApi")
+    private boolean requirePermissions(){
+        for(int i = 0; i<PERMISSONS.length; i++){
+            if(checkSelfPermission(PERMISSONS[i]) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && requirePermissions()){
+            requestPermissions(PERMISSONS, REQUEST_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+         if(requestCode == REQUEST_PERMISSIONS && grantResults.length > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+             if(requirePermissions()){
+                 ((ActivityManager) this.getSystemService(ACTIVITY_SERVICE)).clearApplicationUserData();
+                 recreate();
+             }
+         }
+    }
+
+    private void init(){
+
     }
 }
